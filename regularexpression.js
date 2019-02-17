8279a5ad30325c64cac857b064733b75c1ab76f3
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-17 23:02:50 +0000

    Used | to find several words

--- regularexpression.js
+++ regularexpression.js
@@ -1,11 +1,44 @@
 // Regular expressions are used in programming languages to match parts of strings. You create patterns to help you do that matching. 
 
 
+//Find dog cat bird or fish 
+let petString = "James has a pet cat.";
+let petRegex = /change/; // Change this line
+let result = petRegex.test(petString);
 
-let myString = "Hello, World!";
-let myRegex = /Hello/;
-let result = myRegex; // Change this line
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
 
-let myString = "Hello, World!";
-let myRegex = /Hello/;
-let result = myRegex.test(myString);
+
+
+
+
+
+
+
+
+
+// //Find Waldo
+// let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+// let waldoRegex = /search/; // Change this line
+// let result = waldoRegex.test(waldoIsHiding);
+
+// //Correct Code
+// let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+// let waldoRegex = /Waldo/; 
+// let result = waldoRegex.test(waldoIsHiding);
+
+
+
+
+
+
+//Find Hello
+// let myString = "Hello, World!";
+// let myRegex = /Hello/;
+// let result = myRegex; // Change this line
+
+// let myString = "Hello, World!";
+// let myRegex = /Hello/;
+// let result = myRegex.test(myString);
