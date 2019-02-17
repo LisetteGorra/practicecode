8c207594950a373f8ae9d6afb7fccf2d8f49eb9d
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-17 23:27:48 +0000

    Used g and i flag at the same time to repeat and find match

--- regularexpression.js
+++ regularexpression.js
@@ -2,19 +2,26 @@
 
 
 
-// using .match() method 
-let extractStr = "Extract the word 'coding' from this string.";
-let codingRegex = /change/; // Change this line
-let result = extractStr; // Change this line
-
-let extractStr = "Extract the word 'coding' from this string.";
-let codingRegex = /coding/; // Change this line
-let result = extractStr.match(codingRegex); // Change this line
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /change/; // Change this line
+let result = twinkleStar; // Change this line
 
 
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; 
+let result = twinkleStar.match(starRegex); 
 
 
 
+
+// // using .match() method 
+// let extractStr = "Extract the word 'coding' from this string.";
+// let codingRegex = /change/; // Change this line
+// let result = extractStr; // Change this line
+
+// let extractStr = "Extract the word 'coding' from this string.";
+// let codingRegex = /coding/; // Change this line
+// let result = extractStr.match(codingRegex); // Change this line
 
 
 
