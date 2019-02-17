d6cc64cf2442e32489c855315982dd48e417208f
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-17 22:53:52 +0000

    Used .test method, searched for hello for myRegex

---
+++ regularexpression.js
@@ -0,0 +1,11 @@
+// Regular expressions are used in programming languages to match parts of strings. You create patterns to help you do that matching. 
+
+
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex; // Change this line
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString);
