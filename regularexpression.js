665a25927eb6836218ac577fe3932c5e675b2bb8
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-17 23:00:08 +0000

    matching a literal string, with different possibilities

--- regularexpression.js
+++ regularexpression.js
@@ -1,11 +1,25 @@
 // Regular expressions are used in programming languages to match parts of strings. You create patterns to help you do that matching. 
 
+//Find Waldo
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /search/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
+
+//Correct Code
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; 
+let result = waldoRegex.test(waldoIsHiding);
 
 
-let myString = "Hello, World!";
-let myRegex = /Hello/;
-let result = myRegex; // Change this line
 
-let myString = "Hello, World!";
-let myRegex = /Hello/;
-let result = myRegex.test(myString);
+
+
+
+//Find Hello
+// let myString = "Hello, World!";
+// let myRegex = /Hello/;
+// let result = myRegex; // Change this line
+
+// let myString = "Hello, World!";
+// let myRegex = /Hello/;
+// let result = myRegex.test(myString);
