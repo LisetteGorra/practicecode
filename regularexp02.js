60ca6e14a5c0d1a5580c5dc24e2d42807024e666
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-19 22:15:52 +0000

    Find Characters with Lazy Matching using (?)

--- regularexp02.js
+++ regularexp02.js
@@ -1,9 +1,22 @@
 //Match Characters that Occur Zero or More Times with (*)
 
-let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
-let chewieRegex = /change/; // Change this line
-let result = chewieQuote.match(chewieRegex);
+//Find Characters with Lazy Matching using (?)
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*>/; // Change this line
+let result = text.match(myRegex);
 
-let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
-let chewieRegex = /Aa*/; // Change this line
-let result = chewieQuote.match(chewieRegex);
+
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<h1>?/; // Change this line
+let 
+
+
+
+
+// let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+// let chewieRegex = /change/; // Change this line
+// let result = chewieQuote.match(chewieRegex);
+
+// let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+// let chewieRegex = /Aa*/; // Change this line
+// let result = chewieQuote.match(chewieRegex);
