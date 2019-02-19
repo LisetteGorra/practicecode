ae7cb4d29f7404b9ba38d3662d3ecdf6d2e23ca1
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-19 22:30:20 +0000

    Find One or More Criminals in a Hunt with (+)

--- regularexp02.js
+++ regularexp02.js
@@ -1,14 +1,41 @@
-//Match Characters that Occur Zero or More Times with (*)
+
+
+
+//Find One or More Criminals in a Hunt with (+)
+// example crowd gathering
+let crowd = 'P1P2P3P4P5P6CCCP7P8P9';
+
+let reCriminals = /./; // Change this line
+
+let matchedCriminals = crowd.match(reCriminals);
+console.log(matchedCriminals);
+
+// example crowd gathering
+let crowd = 'P1P2P3P4P5P6CCCP7P8P9';
+
+let reCriminals = /C+/; // Change this line
+
+let matchedCriminals = crowd.match(reCriminals);
+console.log(matchedCriminals);
+
+
+
+
+
+
+
+
+
 
 //Find Characters with Lazy Matching using (?)
-let text = "<h1>Winter is coming</h1>";
-let myRegex = /<.*>/; // Change this line
-let result = text.match(myRegex);
+// let text = "<h1>Winter is coming</h1>";
+// let myRegex = /<.*>/; // Change this line
+// let result = text.match(myRegex);
 
 
-let text = "<h1>Winter is coming</h1>";
-let myRegex = /<h1>?/; // Change this line
-let 
+// let text = "<h1>Winter is coming</h1>";
+// let myRegex = /<h1>?/; // Change this line
+// let 
 
 
 
