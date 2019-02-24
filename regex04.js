330b2899c54f3405941607c89ba6019b98207d44
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-24 20:37:45 +0000

    variable to replace good with okey-dokey.

--- regex04.js
+++ regex04.js
@@ -1,18 +1,19 @@
+//Use Capture Groups to Search and Replace
+let huhText = "This sandwich is good.";
+let fixRegex = /change/; // Change this line
+let replaceText = ""; // Change this line
+let result = huhText.replace(fixRegex, replaceText);
+
+//variable to replace "good" with "okey-dokey".
 //Reuse Patterns Using Capture Groups
 let repeatNum = "42 42 42";
-let reRegex = /change/; // Change this line
+let reRegex = /good/; // Change this line
 let result = reRegex.test(repeatNum);
 
 //this code will match numbers that are repeated only three times in a string, each separated by a space.
 let repeatNum = "42 42 42";
 let reRegex = /^(\d+)\s\1\s\1$/; // Change this line
 let result = reRegex.test(repeatNum);
-
-
-
-
-
-
 
 
 //Positive and Negative Lookahead
