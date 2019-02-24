25a3ace4f23543613f93b1dd0196b27d12ce0506
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-24 20:31:49 +0000

    matching numbers that repeat 3 times and have space in between them

--- regex04.js
+++ regex04.js
@@ -1,9 +1,26 @@
+//Reuse Patterns Using Capture Groups
+let repeatNum = "42 42 42";
+let reRegex = /change/; // Change this line
+let result = reRegex.test(repeatNum);
+
+//this code will match numbers that are repeated only three times in a string, each separated by a space.
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+)\s\1\s\1$/; // Change this line
+let result = reRegex.test(repeatNum);
+
+
+
+
+
+
+
+
 //Positive and Negative Lookahead
 let sampleWord = "astronaut";
 let pwRegex = /change/; // Change this line
 let result = pwRegex.test(sampleWord);
 
-//2 positive lookaheads that match match passwords that are greater than 5 characters long and have two consecutive digits.
+//2 positive lookaheads that match passwords that are greater than 5 characters long and have two consecutive digits.
 let sampleWord = "astronaut";
 let pwRegex = /(?=\w{5,})(?=\D*\d{2})/; // Change this line
 let result = pwRegex.test(sampleWord);
